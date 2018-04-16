0991ed3e77f839abf3e649fa47c747a0b999fc95
Domas Petkevicius <dpet@dbml.dk>
2018-04-16 11:10:52 +0300

    Implement appear.in reloader for Team 417

---
+++ team417/reloader.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Team 417 appear.in channe; reloader
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Reload Team 417 appear.in channel to recover it in case of crashing
+// @author       dpet
+// @match        https://appear.in/june2.0*
+// @grant        none
+// @updateURL   https://raw.githubusercontent.com/domaspe/userscripts/master/team417/reloader.meta.js
+// @downloadURL https://raw.githubusercontent.com/domaspe/userscripts/master/team417/reloader.user.js
+// ==/UserScript==
+
+var url = 'https://appear.in/june2.0';
+var interval = 30 * 60 * 1000; // 30min
+
+setTimeout(function() {
+    window.open(url, '_blank');
+    window.close();
+}, interval);
