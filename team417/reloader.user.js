cae72b15eb53bf0edc841f1aa67f60c9b5ae94c0
Domas Petkevicius <dpet@dbml.dk>
2018-05-28 10:40:36 +0300

    Toggle supersize for the remote tema screen

--- team417/reloader.user.js
+++ team417/reloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Team 417 appear.in channel reloader
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Reload Team 417 appear.in channel to recover it in case of crashing
 // @author       dpet
 // @match        https://appear.in/june2.0*
@@ -13,7 +13,33 @@
 var url = 'https://appear.in/june2.0';
 var interval = 30 * 60 * 1000; // 30min
 
+/**
+ * This part is responsible for setting the reload timetout
+ */
 setTimeout(function() {
     window.open(url, '_blank');
     window.close();
 }, interval);
+
+/**
+ * This part is responsible for clicking on full screen button
+ */
+var checkStarted = setInterval(function() {
+    if (!document.querySelector('.connection-attempt.wrapper') && document.querySelector('.video-stream-container')) {
+        clearInterval(checkStarted);
+
+        var teamNameNode = document.querySelectorAll('.user-settings-info-text')[0];
+        if (!teamNameNode) {
+            return;
+        }
+
+        var teamName = teamNameNode.innerText;
+        var button = Array.from(document.querySelectorAll('p[ng-show="module.user.displayName"]'))
+            .filter(function (item) { return item.innerText !== teamName; })[0]
+            .parentElement
+            .parentElement
+            .querySelector('video-toolbar-button[action="module.toggleSuperSize()"] button.active');
+
+        button.click();
+    }
+ }, 100);
