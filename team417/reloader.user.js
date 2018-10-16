5f29ab2d6f5859cfc25e23d9bb948c5819f21733
Domas Petkevicius <domas.petkevicius@gmail.com>
2018-10-16 10:59:21 +0300

    Disable auto fullscreen

--- team417/reloader.user.js
+++ team417/reloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Team 417 appear.in channel reloader
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Reload Team 417 appear.in channel to recover it in case of crashing
 // @author       dpet
 // @match        https://appear.in/june2.0*
@@ -24,22 +24,22 @@ setTimeout(function() {
 /**
  * This part is responsible for clicking on full screen button
  */
-var checkStarted = setInterval(function() {
-    if (!document.querySelector('.connection-attempt.wrapper') && document.querySelector('.video-stream-container')) {
-        clearInterval(checkStarted);
+// var checkStarted = setInterval(function() {
+//     if (!document.querySelector('.connection-attempt.wrapper') && document.querySelector('.video-stream-container')) {
+//         clearInterval(checkStarted);
 
-        var teamNameNode = document.querySelectorAll('.user-settings-info-text')[0];
-        if (!teamNameNode) {
-            return;
-        }
+//         var teamNameNode = document.querySelectorAll('.user-settings-info-text')[0];
+//         if (!teamNameNode) {
+//             return;
+//         }
 
-        var teamName = teamNameNode.innerText;
-        var button = Array.from(document.querySelectorAll('p[ng-show="module.user.displayName"]'))
-            .filter(function (item) { return item.innerText !== teamName; })[0]
-            .parentElement
-            .parentElement
-            .querySelector('video-toolbar-button[action="module.toggleSuperSize()"] button.active');
+//         var teamName = teamNameNode.innerText;
+//         var button = Array.from(document.querySelectorAll('p[ng-show="module.user.displayName"]'))
+//             .filter(function (item) { return item.innerText !== teamName; })[0]
+//             .parentElement
+//             .parentElement
+//             .querySelector('video-toolbar-button[action="module.toggleSuperSize()"] button.active');
 
-        button.click();
-    }
- }, 100);
+//         button.click();
+//     }
+//  }, 100);
